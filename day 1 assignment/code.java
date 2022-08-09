cb473ac64157e6674c08e8d22dc2ce8d120a40db
Saket <saket.mehta1997@gmail.com>
2022-08-09 15:08:52 +0530

    first commit

---
+++ day 1 assignment/code.java
@@ -0,0 +1,32 @@
+class Point {
+	
+	private double x;
+	private double y;
+	
+
+	public void setX(double x) {
+		this.x = x;
+	}
+	
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	
+	//Method to obtain distance of a point w.r.t origin
+	public double distance(){
+	    //code here
+	}
+	
+	//Method to obtain distance of a point w.r.t another point
+	public double distance(Point point){
+	    //code here
+	}
+
+}
+class Tester {
+	public static void main(String[] args) {
+	    //code here
+    		
+	}
+}
