873fd89fe1405f21dfa6fb9cc5d760b138b21dc0
Saket <saket.mehta1997@gmail.com>
2022-08-22 10:59:39 +0530

    microservices demo added

--- bej_c2_s5_testing_controller_layer_sessiondemo/src/main/java/com/niit/customerservice/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.niit.customerservice.repository;
-
-import com.niit.customerservice.domain.Customer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CustomerRepository extends MongoRepository<Customer,Integer> {
-}
